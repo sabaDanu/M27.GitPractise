ddb4a349d5f72a856fad6e069674ef0bf20c3e9e
saba parveen <psaba2040@gmail>
2025-05-15 12:48:28 +0530

    commit and push again

--- src/test/java/Sample/Demo1.java
+++ src/test/java/Sample/Demo1.java
@@ -1,11 +1,10 @@
 package Sample;
 
-public class Demo {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
-		System.out.println("hello");
+		System.out.println("Hi again");
 	}
 
 }
